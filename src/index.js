a04acd22b533d5d6cb7928bb9e47452fb6eddf0d
jenatayeva <atayevajennet1126@gmail.com>
2022-12-05 19:24:02 +0500

    Last Update

--- src/index.js
+++ src/index.js
@@ -30,10 +30,10 @@ io.on('connection', (socket) => {
 
     socket.emit('message', generatedMessage(`Admin Welcome!`));
     socket.broadcast.to(user.room).emit('message', generatedMessage(`${user.username} has joined`));
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room)
-    })
+    // io.to(user.room).emit('roomData', {
+    //   room: user.room,
+    //   users: getUsersInRoom(user.room)
+    // })
 
     callback()
     // socket.emit, io.emit, socket.broadcast.emit
